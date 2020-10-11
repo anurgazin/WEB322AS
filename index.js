2e1375fcebcaa444d78a73a1e65cc9e6ae2cfbf5
anurgazin <54969694+anurgazin@users.noreply.github.com>
2020-10-11 14:09:58 -0400

    first commit

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-document.addEventListener('DOMContentLoaded', nav)
-function nav(){
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.navbar');
-    burger.addEventListener('click', ()=>{
-        nav.classList.toggle('show')
-    })
+document.addEventListener("DOMContentLoaded", nav);
+function nav() {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".navbar");
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("show");
+  });
 }
